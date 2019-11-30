603a31e829d42186f4817fd261b8363faa261419
Серега Каланджий <kjserg@i.ua>
2019-11-30 22:41:59 +0200

    add validation and scroll btn

--- src/js/app.js
+++ src/js/app.js
@@ -111,6 +111,84 @@ class Application {
             }
 
         });
+
+        $('.scroll-to-js').on('click', function (e) {
+            e.preventDefault();
+
+            const $this = $(this);
+
+            const href = $this.attr('href').replace('#', '');
+
+            const $element = $(href);
+
+            console.log(1);
+
+            if($element.length > 0) {
+
+                $('html, body').animate(
+                    {scrollTop: $element.offset().top},
+                    1000
+                );
+
+            }
+
+        });
+
+        $('form').on('submit', function (e) {
+            e.preventDefault();
+            let ths = $(this),
+                test = true,
+                thsInputs = ths.find('input');
+
+
+            thsInputs.each(function () {
+                let thsInput = $(this),
+                    thsInputType = thsInput.attr('type'),
+                    thsInputVal =  thsInput.val(),
+                    inputReg = new RegExp(thsInput.data('reg')),
+                    inputTest = inputReg.test(thsInputVal);
+
+                if (thsInput.attr('required')) {
+                    if (thsInputVal.length <= 0) {
+                        test = false;
+                        thsInput.addClass('error');
+                        thsInput.focus();
+                    } else {
+                        thsInput.removeClass('error');
+                        if (thsInput.data('reg')) {
+                            if ( inputTest == false ) {
+                                test = false;
+                                thsInput.addClass('error');
+                                thsInput.focus();
+                            } else {
+                                thsInput.removeClass('error');
+                            }
+                        }
+
+                    }
+                }
+            });
+            if ( test ) {
+                let form_data = ths.serialize();
+                $.ajax({
+                    url: ths.attr('action'),
+                    type: 'POST',
+                    data: form_data,
+                    success: function(r) {
+                        $.fancybox.close();
+                        $.fancybox.open($('#modal_thanks'));
+                        ths.trigger('reset');
+                        setTimeout(()=>{
+                            $.fancybox.close();
+                        }, 3000);
+                    },
+                    error:  function(xhr, str){
+                        console.log('Возникла ошибка: ' + xhr.responseCode);
+                    }
+                });
+            }
+
+        });
     };
 
     static detectBrowser() {
